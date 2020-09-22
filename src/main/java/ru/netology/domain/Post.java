3bb00e310e40b1880915db2cf3b6cbf596da722d
Oleg Sedov <olegsedov1994@yandex.ru>
2020-09-22 20:01:18 +0300

    Initial commit

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,13 @@
+package ru.netology.domain;
+
+public class Post {
+    private int views;
+    private String repostButton;
+    private String comments;
+    private String likesButton;
+    private String imageURL;
+    private String text;
+    private String time;
+    private int profileName;
+    private int postID;
+}
